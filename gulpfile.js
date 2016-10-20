8abba8d631c36171866337d8472989d6ee8bff31
John Dillick <jdillick@resourceammirati.com>
2016-10-20 11:47:38 -0400

    Fix relative pathing for stylesheet.

--- gulpfile.js
+++ gulpfile.js
@@ -8,18 +8,20 @@ var prettify = require('gulp-html-prettify');
 var cleanCSS = require('gulp-clean-css');
 var sourcemaps = require('gulp-sourcemaps');
 var del = require('del');
+var path = require('path');
 
 var config = {
   htmlDest: 'docs',
   confluenceDest: 'confluence',
   mdSrc: ['**/*.md', '!node_modules/**', '!bower_components/**'],
   wrapper: {
-    header: '<!DOCTYPE html><html lang="en"><head><link rel="stylesheet" type="text/css" href="/css/splendor.css"></head><body>',
+    header: '<!DOCTYPE html><html lang="en"><head><title>{{TITLE}}</title><link rel="stylesheet" type="text/css" href="{{CSSPATH}}css/splendor.css"></head><body>',
     footer: '</body></html>'
   },
   marked: {
     gfm: true
-  }
+  },
+  title: 'Styleguide/Stylesheet Principles'
 };
 
 gulp.task('build', ['clean', 'confluence', 'html', 'style']);
@@ -38,12 +40,36 @@ gulp.task('confluence', function(){
     .pipe(gulp.dest(config.confluenceDest));
 });
 
+function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
+
 gulp.task('html', function(){
   return gulp.src(config.mdSrc)
     .pipe(debug())
     .pipe(md(config.marked))
     .pipe(replace('.md', '.html'))
-    .pipe(wrapper(config.wrapper))
+    .pipe(wrapper({
+      header: function(file) {
+        var prefix = ''
+        for(var i = 0; i < path.relative(__dirname, file.path).split(/\//).length - 1; i++ ) {
+          prefix += '../';
+        }
+
+        var titleName = toTitleCase(path.parse(file.path).name.replace(/-/, ' '));
+        if (titleName === 'Index') {
+          titleName = '';
+        } else {
+          titleName = '- ' + titleName;
+        }
+
+        var header = config.wrapper.header
+          .replace(/\{\{CSSPATH\}\}/, prefix)
+          .replace(/\{\{TITLE\}\}/, config.title + titleName);
+
+        return header;
+      },
+      footer: config.wrapper.footer}))
     .pipe(prettify({indent_char: ' ', indent_size: 2}))
     .pipe(gulp.dest(config.htmlDest));
 });
